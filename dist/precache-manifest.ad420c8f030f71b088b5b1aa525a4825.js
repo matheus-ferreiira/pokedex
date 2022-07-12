4f5457baf6facd661b05aaaea66d77005afa7a32
Matheus <mthsferreirap@gmail.com>
2022-07-11 22:36:05 -0300

    Editing Navbar

---
+++ dist/precache-manifest.ad420c8f030f71b088b5b1aa525a4825.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24749f68e3c483e6bdcf",
+    "url": "/css/app.e5049187.css"
+  },
+  {
+    "revision": "062584886c1b962f3e3b79f3c1e435de",
+    "url": "/fontawesome/css/all.css"
+  },
+  {
+    "revision": "5c7e27dec58a9845cfcdc4c16f6673de",
+    "url": "/fontawesome/css/all.min.css"
+  },
+  {
+    "revision": "4d2a7af0cb4dff3fef59a2208cdba29b",
+    "url": "/fontawesome/css/fontawesome.min.css"
+  },
+  {
+    "revision": "d661a131af20271e35f64eb9cefe5496",
+    "url": "/fontawesome/webfonts/fa-brands-400.ttf"
+  },
+  {
+    "revision": "cf6b43998affe1b2595c1a63ee0601b0",
+    "url": "/fontawesome/webfonts/fa-brands-400.woff"
+  },
+  {
+    "revision": "04f411f3f6c5167d09e9d398db65d3e1",
+    "url": "/fontawesome/webfonts/fa-brands-400.woff2"
+  },
+  {
+    "revision": "a1a0dd3f7860352537107fda9eb29162",
+    "url": "/fontawesome/webfonts/fa-duotone-900.ttf"
+  },
+  {
+    "revision": "4e32c890deab499d76c66b1ff0c0855f",
+    "url": "/fontawesome/webfonts/fa-duotone-900.woff"
+  },
+  {
+    "revision": "7d1f87704f3a6fbb4b6199ee9e6d05b3",
+    "url": "/fontawesome/webfonts/fa-duotone-900.woff2"
+  },
+  {
+    "revision": "65099b38c4d8507690627608c9617ff6",
+    "url": "/fontawesome/webfonts/fa-light-300.ttf"
+  },
+  {
+    "revision": "e7447ad6a02a8fb0dfa777b45e84dd41",
+    "url": "/fontawesome/webfonts/fa-light-300.woff"
+  },
+  {
+    "revision": "462b09925c9e0d39ead29d072b12615d",
+    "url": "/fontawesome/webfonts/fa-light-300.woff2"
+  },
+  {
+    "revision": "e7dd4ec40a4c6a928b753ba9b7f44709",
+    "url": "/fontawesome/webfonts/fa-regular-400.ttf"
+  },
+  {
+    "revision": "121c9d6df06ce74e86b159342970b30a",
+    "url": "/fontawesome/webfonts/fa-regular-400.woff"
+  },
+  {
+    "revision": "5b2dad1f9126a770b3a1a64056043c08",
+    "url": "/fontawesome/webfonts/fa-regular-400.woff2"
+  },
+  {
+    "revision": "0515d0c4ac818515cf73e3f429bbb921",
+    "url": "/fontawesome/webfonts/fa-solid-900.ttf"
+  },
+  {
+    "revision": "ec05f713ec37819a3ecc516c862e3b23",
+    "url": "/fontawesome/webfonts/fa-solid-900.woff"
+  },
+  {
+    "revision": "27437db6b41f3f3e3884c9a964839bf5",
+    "url": "/fontawesome/webfonts/fa-solid-900.woff2"
+  },
+  {
+    "revision": "7459a124fef2ce0f1f26d4b5ee80839b",
+    "url": "/fontawesome/webfonts/fa-thin-100.ttf"
+  },
+  {
+    "revision": "284e02b328e985f9cc9d1c165b61dcce",
+    "url": "/fontawesome/webfonts/fa-thin-100.woff"
+  },
+  {
+    "revision": "f17c0551e90de6423e55489eefde0cee",
+    "url": "/fontawesome/webfonts/fa-thin-100.woff2"
+  },
+  {
+    "revision": "a1a0dd3f7860352537107fda9eb29162",
+    "url": "/fonts/fa-duotone-900.a1a0dd3f.ttf"
+  },
+  {
+    "revision": "579e25e23113c3308c88ba9bb01475bd",
+    "url": "/img/icon-pokebolas.png"
+  },
+  {
+    "revision": "3140677ef279a72b96a3e2adbc6d8d56",
+    "url": "/img/icon_pokemons.png"
+  },
+  {
+    "revision": "c1f093a64460dcd4c8b5c7f9b9f9d2f4",
+    "url": "/img/logo.png"
+  },
+  {
+    "revision": "55aa8b7e80f5132b8a585852d4514326",
+    "url": "/img/poison.png"
+  },
+  {
+    "revision": "d215a8980738e5e81298fa7d69a3fce1",
+    "url": "/img/wallpaper.d215a898.jpg"
+  },
+  {
+    "revision": "d215a8980738e5e81298fa7d69a3fce1",
+    "url": "/img/wallpaper.jpg"
+  },
+  {
+    "revision": "10af7894e85752873c1579b0c15d25fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24749f68e3c483e6bdcf",
+    "url": "/js/app.2ad99b90.js"
+  },
+  {
+    "revision": "d7d598e804eedd9eed0a",
+    "url": "/js/chunk-vendors.dfc8d2cb.js"
+  },
+  {
+    "revision": "c6db0a64228772c78eafe99538e1ab39",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
